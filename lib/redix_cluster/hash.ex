5e60dc2f22e241ad6467a5aae72959d710fdf5cd
RTLS <carter.e.pedersen@gmail.com>
2024-12-18 12:48:41 -0800

    🚧: Update slot count

--- lib/redix_cluster/hash.ex
+++ lib/redix_cluster/hash.ex
@@ -1,15 +1,14 @@
 defmodule RedixCluster.Hash do
   @moduledoc false
 
-  @redis_cluster_hash_slots 16384
+  @redis_cluster_hash_slots 5461
 
   ## CRCBench
-  # benchmark name   iterations   average time 
+  # benchmark name   iterations   average time
   # CRC                     100   20819.58 µs/op
   # RedixClusterCRC          10   126367.30 µs/op
 
   @spec hash(binary) :: integer
   def hash(key) when is_list(key), do: key |> to_string |> hash
-  def hash(key), do: CRC.ccitt_16_xmodem(key) |>rem(@redis_cluster_hash_slots)
-
+  def hash(key), do: CRC.ccitt_16_xmodem(key) |> rem(@redis_cluster_hash_slots)
 end
--- mix.exs
+++ mix.exs
@@ -2,24 +2,24 @@ defmodule RedixCluster.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :redix_cluster,
-     version: "0.0.2",
-     elixir: "~> 1.6",
-     build_embedded: Mix.env in [:prod],
-     start_permanent: Mix.env == :prod,
-     preferred_cli_env: [espec: :test],
-     deps: deps(),
-     description: description(), 
-     package: package()]
+    [
+      app: :redix_cluster,
+      version: "0.0.2",
+      elixir: "~> 1.6",
+      build_embedded: Mix.env() in [:prod],
+      start_permanent: Mix.env() == :prod,
+      preferred_cli_env: [espec: :test],
+      deps: deps(),
+      description: description(),
+      package: package()
+    ]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [mod: {RedixCluster, []},
-    included_applications: [:crc],
-    applications: [:logger, :redix]]
+    [mod: {RedixCluster, []}, included_applications: [:crc], applications: [:logger, :redix]]
   end
 
   # Dependencies can be Hex packages:
@@ -32,28 +32,26 @@ defmodule RedixCluster.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [ {:redix, "~> 0.9.3"},
+    [
+      {:redix, "~> 1.0"},
       {:poolboy, "~> 1.5"},
       {:dialyze, "~> 0.2", only: :dev},
       {:dogma, "~> 0.0", only: :dev},
       {:crc, "~> 0.5"},
-      {:benchfella, github: "alco/benchfella", only: :bench},
-      {:eredis_cluster, github: "adrienmo/eredis_cluster", only: :bench},
       {:espec, "~> 1.7", only: :test},
-      {:ex_doc, "~> 0.19", only: :dev}, 
+      {:ex_doc, "~> 0.19", only: :dev}
     ]
   end
 
-  defp description do 
+  defp description do
     "A wrapper for redix to support cluster mode of redis"
   end
 
   defp package do
-     [
-       maintainers: ["zhongwencool", "masahiro tokioka", "tinglei8"],
-       licenses: ["MIT"],
-       links: %{"GitHub" => "https://github.com/tinglei8/redix-cluster.git"}
-     ]
+    [
+      maintainers: ["zhongwencool", "masahiro tokioka", "tinglei8"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/tinglei8/redix-cluster.git"}
+    ]
   end
-
 end
